6961cb68b7b03ed95d2dffa39b0b505dfbbc31f5
Rishaad Khan <71447325+rishaadkhan@users.noreply.github.com>
2024-07-18 12:57:28 +0530

    Time: 0 ms (100%), Space: 41.4 MB (39.22%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.java
@@ -1,24 +1,23 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         int circle = 0, square = 0;
-        Stack<Integer> sand = new Stack<>();
-        for(int i = 0; i < students.length; i++){
-            if(students[i] == 0){
+
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == 0) {
                 circle++;
-            }
-            else square++;
-            sand.push(sandwiches[students.length - i - 1]);
+            } else
+                square++;
+
         }
 
-        while(sand.size() > 0){
-            if(sand.peek() == 0 && circle > 0){
-                sand.pop();circle--;
-            }
-            else if (sand.peek() == 1 && square > 0){
-                sand.pop(); square--;
+        for (int i : sandwiches) {
+            if (i == 0 && circle > 0) {
+                circle--;
+            } else if (i == 1 && square > 0) {
+                square--;
 
-            }
-            else break;
+            } else
+                break;
         }
         return circle + square;
     }
